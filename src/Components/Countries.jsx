a6a877262afbb8b914fca417a7329cf97fb19521
dipu-37 <razibul1105nstu@gmail.com>
2024-06-03 22:09:53 +0600

    40-5 Conditional CSS And Conditional Style

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -5,17 +5,31 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setcountries]=useState([]);
+    const [visitedCountry, setvisitedCountry]=useState([])
     useEffect( ()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data =>setcountries(data))
     },[])
+
+const handlevisitedCountry= country =>{
+    console.log('add to your visited country');
+    console.log(country)
+}
+
     return (
         <div >
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>visited countries</h5>
+                <ul>
+
+                </ul>
+            </div>
             <div className="country-containner">
             {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                countries.map(country => <Country key={country.cca3} 
+                country={country} handlevisitedCountry={handlevisitedCountry}></Country>)
             }
             </div>
             
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country}) => {
+const Country = ({country ,handlevisitedCountry}) => {
     console.log(country)
     const {name , flags , cca3}=country;
 
@@ -9,11 +9,15 @@ const Country = ({country}) => {
         setvisited(!visited)
     }
 
+    console.log(handlevisitedCountry);
+
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'visited': 'non-visited'}`}>
             <h3 >name :{name?.common}</h3>
             <img src={flags.png} alt="" />
             <p><small>Code: {cca3}</small></p>
+            <button>Mark visited</button>
+            <br />
             <button onClick={handlevisited}>{visited? 'Visited':'Going'}</button>
             {visited ? 'I have visited this country.':'i want to visite'}
         </div>
